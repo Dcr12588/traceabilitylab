f808b2ed07b222b10a655d0ad2c4b6c3d432ae77
Danny Ramirez <dcr1999jr@gmail.com>
2022-09-22 14:00:02 -0700

    trace

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
@@ -17,6 +21,6 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
                                         
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'))
-})
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
